ef3c077c6525f4435697b9dc2bb57da8fd607711
AdiletAkamtov <adiletakmatovofficial@gmail.com>
2022-11-16 19:34:07 +0600

    refactor: add new file Card and fix scss module and add components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
-import Card from "./components/Card"
-import Header from "./components/Header"
-import Drawer from "./components/Drawer"
+import Card from './components/Card/Card'
+import Header from './components/Header'
+import Drawer from './components/Drawer'
+
+const arr = [
+	{
+		title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+		price: '12 999',
+		imgUrl: '/img/sneakers/1.jpg',
+	},
+	{
+		title: 'Мужские Кроссовки Nike Air Max 270',
+		price: '9999',
+		imgUrl: '/img/sneakers/2.jpg',
+	},
+	{
+		title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+		price: '34 000',
+		imgUrl: '/img/sneakers/3.jpg',
+	},
+	{
+		title: 'Кроссовки Puma X Aka Boku Future Rider',
+		price: '8499',
+		imgUrl: '/img/sneakers/4.jpg',
+	},
+]
 
 function App() {
 	return (
@@ -19,7 +42,9 @@ function App() {
 				</div>
 
 				<div className='d-flex justify-between'>
-					<Card />
+					{arr.map(obj => (
+						<Card title={obj.title} price={obj.price} imgUrl={obj.imgUrl} />
+					))}
 				</div>
 			</div>
 		</div>
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,15 +1,19 @@
 import React from 'react'
+import styles from './Card.module.scss'
 
-const Card = () => {
+const Card = props => {
 	return (
-		<div className='card'>
-			<img width={133} height={112} src='/img/sneakers/4.jpg' alt='' />
-			<h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+		<div className={styles.card}>
+			<div className={styles.favorite}>
+				<img src='/img/heart-unlike.svg' alt='unlike' />
+			</div>
+			<img width={133} height={112} src={props.imgUrl} alt='' />
+			<h5>{props.title}</h5>
 
 			<div className='d-flex justify-between al'>
 				<div className='d-flex flex-column'>
 					<span>Цена:</span>
-					<b>12 990 руб.</b>
+					<b>{props.price} руб.</b>
 				</div>
 				<button className='button'>
 					<img width={11} height={11} src='/img/plus.svg' alt='' />
